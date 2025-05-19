de660a35bb213d16281346448dddc6e8f472c638
lovaraju.mylapalli <mailapallilovaraaju987@gmail.com>
2025-05-19 17:18:58 +0530

    added model wise api access feature

--- models/res_api_key.py
+++ models/res_api_key.py
@@ -12,6 +12,14 @@ class ResApiKey(models.Model):
     expiry_date = fields.Date()
     is_admin = fields.Boolean(string="Admin Access", default=False)
 
+    allowed_model_ids = fields.Many2many(
+        'ir.model',
+        'res_api_key_ir_model_rel',
+        'api_key_id',
+        'model_id',
+        string='Allowed Models'
+    )
+
     def generate_key(self):
         for rec in self:
             rec.key = secrets.token_hex(20)
